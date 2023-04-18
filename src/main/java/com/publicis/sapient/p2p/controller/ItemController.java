9e9f0f540b1d0722e762b467de714eaec0a9c2ce
ravmishr5 <ravmishr5@publicisgroupe.net>
2023-04-18 17:26:48 +0530

    adding api responses on the controller

--- src/main/java/com/publicis/sapient/p2p/controller/ItemController.java
+++ src/main/java/com/publicis/sapient/p2p/controller/ItemController.java
@@ -2,6 +2,13 @@ package com.publicis.sapient.p2p.controller;
 
 import com.publicis.sapient.p2p.entity.Item;
 import com.publicis.sapient.p2p.service.ItemService;
+import com.publicis.sapient.p2p.vo.ServiceResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +21,18 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@Tag(name = "Item", description = "Product Service API for getting item information")
 public class ItemController {
     private final Logger logger = LoggerFactory.getLogger(ItemController.class);
 
     @Autowired
     private ItemService itemService;
 
+    @Operation(operationId = "getItemDetails", description = "Get Item Details with item id", summary = "This endpoint gives item details with given item id with separated with ,", tags = {"Item"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Fetched Items Successfully", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Item.class)))),
+                    @ApiResponse(responseCode = "404", description = "Item Not Found", content = @Content(schema = @Schema(implementation = ServiceResponse.class))),
+            })
     @GetMapping(value = "/item")
     public List<Item> getItemDetails(@RequestParam String id){
         logger.info("Entering getItemDetails method with endpoint: /item");
--- src/main/java/com/publicis/sapient/p2p/controller/ProductController.java
+++ src/main/java/com/publicis/sapient/p2p/controller/ProductController.java
@@ -2,6 +2,13 @@ package com.publicis.sapient.p2p.controller;
 
 import com.publicis.sapient.p2p.model.Product;
 import com.publicis.sapient.p2p.service.ProductService;
+import com.publicis.sapient.p2p.vo.ServiceResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@Tag(name = "Product", description = "Product Service API for getting product details")
 public class ProductController {
 
    private final Logger logger = LoggerFactory.getLogger(ProductController.class);
@@ -19,6 +27,11 @@ public class ProductController {
    @Autowired
    private ProductService productService;
 
+    @Operation(operationId = "getProductDetails", description = "Return all the items falling under Respective Product ID with additional product details", summary = "This endpoint fetches  details of all the items  falling under particular product", tags = {"Product"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Product details Fetched Successfully", content = @Content(array=@ArraySchema( schema = @Schema(implementation = Product.class)))),
+                    @ApiResponse(responseCode = "404", description = "Product Not Found", content = @Content(schema = @Schema(implementation = ServiceResponse.class))),
+            })
    @GetMapping(value = "/product")
    public List<Product> getProductDetails(@RequestParam String id){
        logger.info("Entering getProductDetails method with endpoint: /product");
